cfa949baf48c6a16c60dfd44c03f37b859fc3665
MAHIMA JhADE <mahimajhade123@gmail.com>
2023-01-27 12:27:34 +0530

    uber

--- src/main/java/com/driver/services/impl/CustomerServiceImpl.java
+++ src/main/java/com/driver/services/impl/CustomerServiceImpl.java
@@ -45,7 +45,7 @@ public class CustomerServiceImpl implements CustomerService {
 	public TripBooking bookTrip(int customerId, String fromLocation, String toLocation, int distanceInKm) throws Exception{
 		//Book the driver with lowest driverId who is free (cab available variable is Boolean.TRUE). If no driver is available, throw "No cab available!" exception
 		//Avoid using SQL query
-         Driver bookdriver=new Driver();
+         Driver bookdriver=null;
 
 		List<Driver> driverList=driverRepository2.findAll();
 
