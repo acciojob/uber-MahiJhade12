7edaf61db1db26d1282a4cdd1d18a652389e142a
MAHIMA JhADE <mahimajhade123@gmail.com>
2023-01-27 12:20:25 +0530

    Uber1

--- src/main/java/com/driver/services/impl/CustomerServiceImpl.java
+++ src/main/java/com/driver/services/impl/CustomerServiceImpl.java
@@ -59,7 +59,7 @@ public class CustomerServiceImpl implements CustomerService {
 		}
 
 		if(bookdriver==null){
-			throw new Exception("No cab available!");
+			throw new Exception("No value present");
 		}
 
 		Customer customer= customerRepository2.findById(customerId).get();
